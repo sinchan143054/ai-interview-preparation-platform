b8a22b2162ddcd8b8ea2a84d3ac99e8763fa2367
sinchan143054 <sinchan.143054@gmail.com>
2026-02-13 00:13:03 +0530

    final fix api base

--- backend/models/Interview.js
+++ backend/models/Interview.js
@@ -2,43 +2,22 @@ const mongoose = require("mongoose");
 
 const interviewSchema = new mongoose.Schema({
   userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true
+    type: String,
+    default: "demoUser"
   },
-
   domain: String,
   difficulty: String,
-
   questionIds: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Question"
-    }
+    { type: mongoose.Schema.Types.ObjectId, ref: "Question" }
   ],
-
   currentQuestionIndex: {
     type: Number,
     default: 0
   },
-
   status: {
     type: String,
-    default: "ongoing" // ongoing | completed
-  },
-
-  overallScore: {
-    type: Number,
-    default: 0
-  },
-
-  skillSummary: {
-    technical: { type: Number, default: 0 },
-    communication: { type: Number, default: 0 },
-    problemSolving: { type: Number, default: 0 },
-    confidence: { type: Number, default: 0 }
+    default: "ongoing"
   }
-
-}, { timestamps: true });
+});
 
 module.exports = mongoose.model("Interview", interviewSchema);
